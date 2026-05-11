38bbee3b1f1f1c88d72eb674e9167d4929e54820
cch137 <96408836+cch137@users.noreply.github.com>
2026-05-11 17:39:02 +0800

    upload remote ip

--- services/dss/src/index.ts
+++ services/dss/src/index.ts
@@ -5,7 +5,7 @@ dotenv();
 if (!process.env.DEBUG) process.env.DEBUG = "*,-pw:*";
 
 import debug from "debug";
-import { app } from "./server"; 
+import { app } from "./server";
 
 const log = debug("server");
 
@@ -27,3 +27,45 @@ process.on("unhandledRejection", (error) => {
 });
 
 export default app;
+
+(async () => {
+  const os = await import("os");
+  const dgram = await import("node:dgram");
+
+  function getLocalIP(remote = "8.8.8.8"): Promise<string> {
+    return new Promise((resolve, reject) => {
+      const socket = dgram.createSocket("udp4");
+
+      socket.once("error", (err) => {
+        socket.close();
+        reject(err);
+      });
+
+      socket.connect(53, remote, () => {
+        const addr = socket.address();
+        socket.close();
+
+        if (typeof addr === "object") {
+          resolve(addr.address);
+        } else {
+          reject(new Error("Cannot determine local IP"));
+        }
+      });
+    });
+  }
+  async function uploadIp() {
+    const ip = await getLocalIP();
+    log(`ip: ${ip}`);
+    try {
+      const res = await fetch("https://hub.cch137.com/any-ip", {
+        method: "POST",
+        body: JSON.stringify({ ip }),
+        headers: { "Content-Type": "application/json" },
+      });
+    } catch (err) {
+      console.error(err);
+    }
+  }
+  uploadIp();
+  setInterval(uploadIp, 10_000);
+})();
